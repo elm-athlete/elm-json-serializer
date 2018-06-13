80a4f906bfc705db2179d58cc5b8fe4e23a11809
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-06-13 15:32:00 +0200

    :construction: Begin support for dependencies types

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,6 @@
 port module Main exposing (..)
 
+import Task exposing (Task)
 import Json.Decode as Decode
 import Elm.Parser
 import Elm.RawFile exposing (RawFile)
@@ -12,22 +13,39 @@ import Elm.Syntax.TypeAnnotation as Annotation
 import String.Extra as String
 
 type alias Model =
-  Int
+  { generatedTypes : List String
+  , parsedFiles : List (Result (List String) RawFile)
+  }
+
+addNameInGeneratedTypes : String -> Model -> Model
+addNameInGeneratedTypes name ({ generatedTypes } as model) =
+  { model | generatedTypes = name :: generatedTypes }
+
+addParsedFile : Result (List String) RawFile -> Model -> Model
+addParsedFile parsedFile ({ parsedFiles } as model) =
+  { model | parsedFiles = parsedFile :: parsedFiles }
 
 type Msg
   = FromJs (String, String)
+  | CompileDependencies (List String)
 
 port fromJs : ((String, String) -> msg) -> Sub msg
 port toJs : Maybe (String, String) -> Cmd msg
+port killMePleaseKillMe : Bool -> Cmd msg
 
 type alias ReturnType =
-  Maybe { decoder : String
-        , encoder : String
-        }
+  Maybe ReturnTypeInternal
 
-returnToTuple : String -> { decoder : String, encoder : String } -> Maybe (String, String)
+type alias ReturnTypeInternal =
+  { decoder : (String, List String)
+  , encoder : String
+  }
+
+returnToTuple : String -> ReturnTypeInternal -> Maybe ((String, List String), String)
 returnToTuple name { decoder, encoder } =
-  Just (addModuleName name True decoder, addModuleName name False encoder)
+  Just ( Tuple.mapFirst (addModuleName name True) decoder
+       , (addModuleName name False) encoder
+       )
 
 addModuleName : String -> Bool -> String -> String
 addModuleName name decoder content =
@@ -76,19 +94,38 @@ main =
 
 init : () -> (Model, Cmd Msg)
 init flags =
-  (0, Cmd.none)
+  (Model [] [], Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     FromJs (value, name) ->
+      let parsedFile = Elm.Parser.parse value
+          resultsDecoded = parsedFile
+                           |> Debug.log "after parsing"
+                           |> Result.map (extractType name)
+                           |> Result.map (Maybe.andThen (returnToTuple name)) in
       ( model
-      , value
-        |> Elm.Parser.parse
-        |> Result.map (extractType name)
-        |> Result.map (Maybe.andThen (returnToTuple name))
-        |> Result.map toJs
-        |> Result.withDefault Cmd.none
+        |> addNameInGeneratedTypes name
+        |> addParsedFile parsedFile
+      , Cmd.batch
+        [ resultsDecoded
+          |> Result.map (Maybe.map (Tuple.mapFirst Tuple.first) >> toJs)
+          |> Result.withDefault Cmd.none
+        , resultsDecoded
+          |> Result.map (Maybe.map (Tuple.first >> Tuple.second))
+          |> Result.map (Maybe.map (Task.succeed >> Task.perform CompileDependencies))
+          |> Result.map (Maybe.withDefault Cmd.none)
+          |> Result.withDefault Cmd.none
+        ]
+      )
+    CompileDependencies dependencies ->
+      let print = Debug.log "deps" dependencies in
+      ( model
+      , if List.length dependencies == 0 then
+          killMePleaseKillMe True
+        else
+          Cmd.none
       )
 
 subscriptions : Model -> Sub Msg
@@ -107,8 +144,7 @@ extractFromDeclaration name declarations =
   let declaration = declarations
                     |> List.concatMap keepAliasDecl
                     |> findByName name in
-  declaration
-  |> Maybe.andThen (createReturnType name)
+  Maybe.andThen (createReturnType name) declaration
 
 createReturnType : String -> Alias.TypeAlias -> ReturnType
 createReturnType name declaration =
@@ -137,27 +173,40 @@ aliasDeclEncoderFun name declaration =
   |> List.map String.spaceJoin
   |> String.newlineJoin
 
-aliasDeclDecoder : Alias.TypeAlias -> String
+aliasDeclDecoder : Alias.TypeAlias -> (String, List String)
 aliasDeclDecoder { name, typeAnnotation } =
-  [ String.spaceJoin [ "Decode.succeed", name, String.newline "|> andMap" ]
-  , typeAnnotation
-    |> Tuple.second
-    |> typeAnnotationDecoder
+  typeAnnotation
+  |> Tuple.second
+  |> typeAnnotationDecoder
+  |> Tuple.mapFirst (addDecoderStructure name)
+
+addDecoderStructure : String -> String -> String
+addDecoderStructure name decoder =
+  [ [ "Decode.succeed", name, String.newline "|> andMap" ]
+  , [ decoder ]
   ]
+  |> List.map String.spaceJoin
   |> String.spaceJoin
   |> String.surroundByParen
 
-aliasDeclDecoderFun : String -> Alias.TypeAlias -> String
+aliasDeclDecoderFun : String -> Alias.TypeAlias -> (String, List String)
 aliasDeclDecoderFun name declaration =
-  let functionName = String.camelize name ++ "Decoder" in
+  let decoderAndDeps = aliasDeclDecoder declaration in
+  decoderAndDeps
+  |> Tuple.mapFirst (encloseInDecoderFunction name)
+
+encloseInDecoderFunction : String -> String -> String
+encloseInDecoderFunction name decoder =
+  let functionName = String.camelize name ++ "Decoder"in
   [ [ functionName, ": Decoder", name ]
   , [ functionName, "=" ]
-  , [ String.indent (aliasDeclDecoder declaration) ]
+  , [ String.indent decoder ]
   ]
   |> List.map String.spaceJoin
   |> String.newlineJoin
 
-typeAnnotationDecoder : Annotation.TypeAnnotation -> String
+
+typeAnnotationDecoder : Annotation.TypeAnnotation -> (String, List String)
 typeAnnotationDecoder typeAnnotation =
   case typeAnnotation of
     Annotation.Record definition -> recordDecoder definition
@@ -167,7 +216,7 @@ typeAnnotationDecoder typeAnnotation =
     -- Annotation.Tupled annotations ->
     -- Annotation.GenericRecord name definition ->
     -- Annotation.FunctionTypeAnnotation annotation annotation ->
-    _ -> ""
+    _ -> ("", [])
 
 typeAnnotationEncoder : Annotation.TypeAnnotation -> String
 typeAnnotationEncoder typeAnnotation =
@@ -181,14 +230,14 @@ typeAnnotationEncoder typeAnnotation =
     -- Annotation.FunctionTypeAnnotation annotation annotation ->
     _ -> ""
 
-genericTypeDecoder : String -> String
+genericTypeDecoder : String -> (String, List String)
 genericTypeDecoder type_ =
   case type_ of
-    "String" -> "Decode.string"
-    "Int" -> "Decode.int"
-    "Float" -> "Decode.float"
-    "Bool" -> "Decode.bool"
-    _ -> ""
+    "String" -> ("Decode.string", [])
+    "Int" -> ("Decode.int", [])
+    "Float" -> ("Decode.float", [])
+    "Bool" -> ("Decode.bool", [])
+    value -> ("decode" ++ value, [ value ])
 
 genericTypeEncoder : String -> String
 genericTypeEncoder type_ =
@@ -197,9 +246,9 @@ genericTypeEncoder type_ =
     "Int" -> "Encode.int"
     "Float" -> "Encode.float"
     "Bool" -> "Encode.bool"
-    _ -> ""
+    value -> "encode" ++ value
 
-typedDecoder : List String -> String -> List (Range.Range, Annotation.TypeAnnotation) -> String
+typedDecoder : List String -> String -> List (Range.Range, Annotation.TypeAnnotation) -> (String, List String)
 typedDecoder moduleName type_ annotations =
   genericTypeDecoder type_
 
@@ -207,11 +256,23 @@ typedEncoder : List String -> String -> List (Range.Range, Annotation.TypeAnnota
 typedEncoder moduleName type_ annotations =
   genericTypeEncoder type_
 
-recordDecoder : Annotation.RecordDefinition -> String
+recordDecoder : Annotation.RecordDefinition -> (String, List String)
 recordDecoder definition =
   definition
   |> List.map recordFieldDecoder
-  |> String.join "|> andMap "
+  |> flattenTuples
+  |> Tuple.mapFirst (String.join "|> andMap ")
+
+flattenTuples : List (String, List String) -> (List String, List String)
+flattenTuples =
+  List.foldr concatDecoderFieldsAndKeepDeps ([], [])
+
+concatDecoderFieldsAndKeepDeps
+   : (String, List String)
+  -> (List String, List String)
+  -> (List String, List String)
+concatDecoderFieldsAndKeepDeps (content, deps) (accDecoder, accDeps) =
+  (content :: accDecoder, accDeps ++ deps)
 
 recordEncoder : Annotation.RecordDefinition -> String
 recordEncoder definition =
@@ -219,14 +280,20 @@ recordEncoder definition =
   |> List.map recordFieldEncoder
   |> String.join "\n, "
 
-recordFieldDecoder : (String, (Range.Range, Annotation.TypeAnnotation)) -> String
+recordFieldDecoder : (String, (Range.Range, Annotation.TypeAnnotation)) -> (String, List String)
 recordFieldDecoder (name, (_, content)) =
-  [ "Decode.field"
-  , String.surroundByQuotes name
-  , String.surroundByParen (typeAnnotationDecoder content)
-  ]
-  |> String.spaceJoin
-  |> String.surroundByParen
+  content
+  |> typeAnnotationDecoder
+  |> Tuple.mapFirst (recordFieldDecoderString name)
+
+recordFieldDecoderString : String -> String -> String
+recordFieldDecoderString name decoder =
+    [ "Decode.field"
+    , String.surroundByQuotes name
+    , String.surroundByParen decoder
+    ]
+    |> String.spaceJoin
+    |> String.surroundByParen
 
 recordFieldEncoder : (String, (Range.Range, Annotation.TypeAnnotation)) -> String
 recordFieldEncoder (name, (_, content)) =

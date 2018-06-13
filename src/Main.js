d9c2034327f1853c21666d0379bd2463431bfb68
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-06-13 13:06:03 +0200

    :recycle: Improve naming

--- src/Main.js
+++ src/Main.js
@@ -2,13 +2,13 @@ const { Elm } = require('../dist/app.js')
 const app = Elm.Main.worker()
 
 const filePath = process.argv[2]
-const type = process.argv[3]
+const name = process.argv[3]
 const cwd = process.cwd()
 const completeFilePath = cwd + '/' + filePath
 
 const fs = require('fs')
 const readedFile = fs.readFile(completeFilePath, (err, res) => {
-  app.ports.fromJs.send([ res.toString(), type ])
+  app.ports.fromJs.send([ res.toString(), name ])
 })
 
 app.ports.toJs.subscribe(decoderAndEncoder => {
